f78292b5a84413f95b50d36812e6ea13212ab3e0
drebyrd715 <drebyrd715@gmail.com>
2023-10-22 17:56:52 -0400

    final update

--- statichw1.js
+++ statichw1.js
@@ -1,55 +1,76 @@
-class Governor { 
-    static control = true
-    static intelligence = true 
-    
-
-static showAuthority(){console.log("Show Authority")}
-
-static outSmartYou(){console.log("I play chess not checkers")}
-
-}
-// const auth = new Governor()
-// console.log(Governor.control)
-// console.log( Governor.intelligence)
-// Governor.showAuthority()
-// Governor.outSmartYou()
-
-
-
 class Person {
     constructor(name, age, hairColor,) {
         this.name = name
         this.age = age
         this.hairColor = hairColor
     }
-  static hairColors() {
-    return ["green", "pink", "grey"];
-  }
 
 }
-
-class Worker extends Person {
+class Governor extends Person { 
+    constructor(name, age, hairColor, state) {
+        super(name, age, hairColor,)
+        this.name = name
+        this.age = age
+        this.hairColor = hairColor
+        this.state = state
+    }
+    
+    static hairColors() {
+        return ["green", "pink", "grey"];
+      }
+    static state = true
+    
+static state(){console.log("Show Authority")}
 
 }
+const Gov1 = new Governor("Dre", 52, "grey", "Pennsylvania")
+// console.log(Gov1.state)
+// console.log(Gov1)
 
-const postalWorker = new Worker(
-  "Rocky Balboa",
-  47,
-  Person.hairColors()[2],
-  "grey"
-);
 
-console.log(postalWorker)
-
-class privateCook extends Person {
-
+class PostalWorker extends Person {
+    constructor(name, age, hairColor, longHours) {
+       super (name, age, hairColor)
+       this.name = name
+       this.age = age
+       this.hairColor = hairColor
+       this.longHours = longHours
+        
+    }
+    steal() {
+     console.log("lets get away!");
+    }
+    greeting() {
+    console.log("Good day sir");
+    }      
 }
 
-const chef = new privateCook(
-  "Clark Kent",
-  30,
-  Person.hairColors()[0],
-  "green"
-);
+ const worker1 = new PostalWorker("RockyBalboa", 47, "green", 50)
+      
+// console.log(worker1)
+// worker1.steal()
+// worker1.greeting()
+// console.log(worker1.hairColor)
 
-console.log(chef)
+class Chef extends Person {
+    constructor(name, age, hairColor, specialSause) {
+        super (name, age, hairColor) 
+        this.name = name
+        this.age = age
+        this.hairColor = hairColor
+        this. specialSause = specialSause
+    }
+    season() {
+        console.log("Put my foot in it!");
+       }
+       tasttest() {
+       console.log("That was delicious");
+       }      
+}
+
+const privateCook = new Chef ("Jarrod", 18, "black", "Grandma's Sause")
+
+console.log(privateCook)
+console.log(privateCook.specialSause)
+privateCook.season()
+privateCook.tasttest()
